a76a01881b21b10e73b43fd9b9aa074bcab0cbb2
galshaya <42077171+galshaya@users.noreply.github.com>
2025-05-19 20:09:01 -0400

    easter egg

---
+++ src/app/metaverse-pappi/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+export default function MetaversePappi() {
+  return (
+    <div>
+      <iframe
+        src="https://proto-tech-club-viz.netlify.app/"
+        className="w-full h-screen border-0"
+        title="Metaverse Pappi"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
+}
