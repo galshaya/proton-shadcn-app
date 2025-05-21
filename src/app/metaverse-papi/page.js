109f8b00422d353e4e97a3d9c83f5ee2ad48b26e
galshaya <42077171+galshaya@users.noreply.github.com>
2025-05-21 09:12:08 -0400

    yez

--- src/app/metaverse-papi/page.js
+++ src/app/metaverse-papi/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-export default function MetaversePappi() {
+export default function MetaversePapi() {
   return (
     <div>
-      <iframe
-        src="https://proto-tech-club-viz.netlify.app/"
+      <iframe 
+        src="https://proto-tech-club-viz.netlify.app/" 
         className="w-full h-screen border-0"
-        title="Metaverse Pappi"
+        title="Metaverse Papi"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       />
